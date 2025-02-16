986b5b12f63eed0bd32fe3a8b79395bd65fa3f71
Karimo21 <karimamaayou20@gmail.com>
2025-02-16 21:50:14 +0100

    added the login and the bottom navigation

--- frontend/lib/screens/home/main_screen.dart
+++ frontend/lib/screens/home/main_screen.dart
@@ -1,28 +1,47 @@
+import 'package:bladnaservices/screens/home/map/explorer_screen.dart';
+import 'package:bladnaservices/screens/home/profile/profil_screen.dart';
+import 'package:bladnaservices/screens/home/reservation/suivi_screen.dart';
+import 'package:bladnaservices/screens/home/services/acceuil_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:bladnaservices/screens/home/chat/chat_list_screen.dart';
+import 'package:bladnaservices/widgets/bottom_navigation.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: EmptyPage(),
-    );
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  // A list of widgets representing each page/screen.
+  final List<Widget> _screens = [
+    AcceuilScreen(), // Tab 1: Acceuil
+    ExplorerScreen(), // Tab 2: Explorer
+    SuiviScreen(),   // Tab 3: Suivi
+    ChatListScreen(),   // Tab 4: Message  
+    ProfilScreen(), // Tab 5: Profil
+ 
+  ];
+
+  // This function is called when a tab is tapped.
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
-}
-
-class EmptyPage extends StatelessWidget {
-  const EmptyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(''),
+    return Scaffold(
+      // The body shows the currently selected screen.
+      body: _screens[_selectedIndex],
+      // The BottomNavigationBar is your custom widget.
+      bottomNavigationBar: BottomNavigation(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
   }
