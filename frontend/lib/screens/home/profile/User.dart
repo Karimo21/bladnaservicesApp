5d151edcd0631806bf9f1e5f0ba20032ff572118
Karimo21 <karimamaayou20@gmail.com>
2025-02-20 11:51:45 +0100

    changed the User class

--- frontend/lib/screens/home/profile/User.dart
+++ frontend/lib/screens/home/profile/User.dart
@@ -2,16 +2,30 @@ class User {
   // Static variables to store user data globally
   static int userId =0;
   static String role = '';
+  static String profile ="";
+  static String fname="";
+  static String lname="";
+  static String ville="";
+  static String description="";
+  static String adresse="";
 
   // Function to set the user data
-  static void setUserData(int id, String userRole) {
+  static void setUserData(int id, String userRole,String profilePicture,String fname1,String lname1,String adresse1,String description1 ) {
     userId = id;
     role = userRole;
+    profile=profilePicture;
+    fname =fname1;
+    lname=lname1;
+    adresse=adresse1;
+    description=description1;
   }
 
   // Function to get the userId
   static int getUserId() {
     return userId;
+  }
+  static setProfile(String profile){
+     profile=profile;
   }
 
   // Function to get the user role
